73732039d42c54a872ea016b0a07ba7b2d9c53b1
yungchi-su <110951034+yungchi-su@users.noreply.github.com>
2024-10-09 15:18:56 +0800

    Update rds.tf

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -16,7 +16,7 @@ resource "aws_rds_cluster" "app1-rds-cluster" {
 
 resource "aws_rds_cluster" "app2-rds-cluster" {
   cluster_identifier      = "app2-rds-cluster"
-  allocated_storage       = 10
+  allocated_storage       = 11
   backup_retention_period = 1
   tags = {
     git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
